e0a191fc22127b83f03da94823f362a2acefb1cb
insile <in.silence@outlook.com>
2024-02-29 11:00:32 +0800

    Quartz sync: Feb 29, 2024, 11:00 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 公共笔记库",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
